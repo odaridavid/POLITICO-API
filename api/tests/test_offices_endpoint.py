7e4c835d81f858001274f98d3025e0a6fc37aab5
Davidodari <davidkibzodari@gmail.com>
2019-02-04 22:30:34 +0300

    [Chore #163684062] Add Comments to each test method

--- api/tests/test_offices_endpoint.py
+++ api/tests/test_offices_endpoint.py
@@ -3,25 +3,25 @@ from . import BaseTestCase
 
 class OfficeEndpointsTestCase(BaseTestCase):
     def test_create_office(self):
-        """"""
+        """Tests POST Http method request on /office endpoint"""
         pass
 
     def test_create_office_bad_request(self):
-        """"""
+        """Tests malformed POST Http method request on /office endpoint"""
         pass
 
     def test_view_all_offices(self):
-        """"""
+        """Tests GET Http method request on /office endpoint"""
         pass
 
     def test_view_all_offices_bad_request(self):
-        """"""
+        """Tests malformed GET Http method request on /office endpoint"""
         pass
 
     def test_view_specific_office(self):
-        """"""
+        """Tests GET Http method request on /office/{:id} endpoint"""
         pass
 
     def test_view_specific_office_not_found(self):
-        """"""
+        """Tests malformed GET Http method request on /office/{:id} endpoint"""
         pass
--- api/tests/test_parties_endpoint.py
+++ api/tests/test_parties_endpoint.py
@@ -3,41 +3,41 @@ from . import BaseTestCase
 
 class PartiesEndpointsTestCase(BaseTestCase):
     def test_create_political_party(self):
-        """"""
+        """Tests POST Http method request on /parties endpoint"""
         pass
 
     def test_create_political_party_bad_request(self):
-        """"""
+        """Tests malformed POST Http method request on /parties endpoint"""
         pass
 
     def test_edit_political_party(self):
-        """"""
+        """Tests PATCH Http method request on /parties/{:id}/name endpoint"""
         pass
 
     def test_edit_political_party_bad_request(self):
-        """"""
+        """Tests malformed PATCH Http method request on /parties/{:id}/name endpoint"""
         pass
 
     def test_delete_political_party(self):
-        """"""
+        """Tests DELETE Http method request on /parties/{:id} endpoint"""
         pass
 
     def test_delete_political_party_not_found(self):
-        """"""
+        """"Tests malformed DELETE Http method request on /parties/{:id} endpoint"""
         pass
 
     def test_view_political_party(self):
-        """"""
+        """Tests GET Http method request on /parties/{:id} endpoint"""
         pass
 
     def test_view_political_party_not_found(self):
-        """"""
+        """Tests malformed GET Http method request on /parties/{:id} endpoint"""
         pass
 
     def test_view_all_political_parties(self):
-        """"""
+        """Tests GET Http method request on /parties/ endpoint"""
         pass
 
     def test_view_all_political_parties_bad_request(self):
-        """"""
+        """Tests malformed GET Http method request on /parties/ endpoint"""
         pass
