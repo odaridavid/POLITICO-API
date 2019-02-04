e2993b826fd498634d9e7d37f46faf9f3b7e97f5
Davidodari <davidkibzodari@gmail.com>
2019-02-04 14:24:28 +0300

    [Starts #163684062] Setup Configuration
    - Create config file that will be used for configuration in app factory
    - Add test configuration in config file

---
+++ api/__init__.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from instance.config import app_config
+
+
+def create_app(config):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    # Setup configuration
+    app.config.from_object(app_config[config])
