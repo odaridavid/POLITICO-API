ca984a65120117bc517a24f5d3ff733c38868731
Davidodari <davidkibzodari@gmail.com>
2019-02-05 13:06:50 +0300

    [Finishes #163723070] Refactor Tests
    * gitignore - to remove cache

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,8 +1,6 @@
 import unittest
 # Imports create app function to set testing config
 from api import create_app
-from .test_parties_endpoint import PartiesEndpointsTestCase
-from .test_offices_endpoint import OfficeEndpointsTestCase
 
 
 class BaseTestCase(unittest.TestCase):
@@ -23,27 +21,3 @@ class BaseTestCase(unittest.TestCase):
             'hqAddress': 'Nairobi,Kenya 00100',
             'logoUrl': 'https://www.some.url.co.ke'
         }
-
-
-if __name__ == '__main__':
-    unittest.main()
-    # Political Parties Test Cases
-    parties = PartiesEndpointsTestCase()
-    parties.test_create_political_party()
-    parties.test_create_political_party_bad_request()
-    parties.test_delete_political_party()
-    parties.test_delete_political_party_not_found()
-    parties.test_edit_political_party()
-    parties.test_edit_political_party_bad_request()
-    parties.test_view_all_political_parties()
-    parties.test_view_all_political_parties_bad_request()
-    parties.test_view_political_party()
-    parties.test_view_political_party_not_found()
-    # Government Ofice Test Cases
-    offices = OfficeEndpointsTestCase()
-    offices.test_create_office()
-    offices.test_create_office_bad_request()
-    offices.test_view_all_offices()
-    offices.test_view_all_offices_bad_request()
-    offices.test_view_specific_office()
-    offices.test_view_specific_office_not_found()
--- api/tests/test_offices_endpoint.py
+++ api/tests/test_offices_endpoint.py
@@ -19,8 +19,8 @@ class OfficeEndpointsTestCase(BaseTestCase):
         # makes JSON Object a string
         expected_data_json = json.dumps(expected_response_data)
         # Assert - (expected,actual)
-        self.assertEqual(201, response.status, "Should Return a 201 HTTP Status Code Response:Created Successfully")
-        self.assertEqual(expected_data_json, str(response.data))
+        assert 201 == response.status, "Should Return a 201 HTTP Status Code Response:Created Successfully"
+        assert expected_data_json == str(response.data)
 
     def test_create_office_bad_request(self):
         """Tests malformed POST Http method request on /offices endpoint"""
@@ -30,9 +30,9 @@ class OfficeEndpointsTestCase(BaseTestCase):
                                         # Missing type key
                                         'name': 'Permanent Secretary'
                                     })
-        self.assertEqual(400, response.status, "Should Return a 400 HTTP Status Code Response:Bad Request")
+        assert 400 == response.status, "Should Return a 400 HTTP Status Code Response:Bad Request"
         # Should return error message
-        self.assertIn("Bad Request", str(response.error), "Should return bad request response")
+        assert "Bad Request" in str(response.error), "Should return bad request response"
 
     def test_view_all_offices(self):
         """Tests GET Http method request on /offices endpoint"""
@@ -41,16 +41,16 @@ class OfficeEndpointsTestCase(BaseTestCase):
         # Retrieve the office
         response = self.client.get('/offices')
         # Assert - (expected,actual)
-        self.assertEqual(200, response.status, "Should Return a 200 HTTP Status Code Response:Success")
+        assert 200 == response.status, "Should Return a 200 HTTP Status Code Response:Success"
         # Converts to string
-        self.assertIn(json.dumps({'data': [self.office]}), str(response.data))
+        assert json.dumps({'data': [self.office]}) in str(response.data)
 
     def test_view_all_offices_bad_request(self):
         """Tests malformed GET Http method request on /office endpoint"""
         response = self.client.get('/ofices')
-        self.assertEqual(404, response.status, "Should Return a 404 HTTP Status Code Response:Not Found")
+        assert 404 == response.status, "Should Return a 404 HTTP Status Code Response:Not Found"
         # Should return error message
-        self.assertIn("Not Found", str(response.error), "Should return resource not found response")
+        assert "Not Found" in str(response.error), "Should return resource not found response"
 
     def test_view_specific_office(self):
         """Tests GET Http method request on /office/{:id} endpoint"""
@@ -58,13 +58,13 @@ class OfficeEndpointsTestCase(BaseTestCase):
         self.client.post('/offices', json=self.office)
         # Get data for specific office
         response = self.client.get('/offices/{0}'.format(self.office['id']))
-        self.assertEqual(200, response.status, "Should Return a 200 HTTP Status Code:Success")
+        assert 200 == response.status, "Should Return a 200 HTTP Status Code:Success"
         # Returns Dict as string and compares if its in response
-        self.assertIn(json.dumps({'data': [self.office]}), str(response.data))
+        assert json.dumps({'data': [self.office]}) in str(response.data)
 
     def test_view_specific_office_not_found(self):
         """Tests malformed GET Http method request on /office/{:id} endpoint"""
         response = self.client.get('/offices/45789')
-        self.assertEqual(404, response.status, "Should Return a 404 HTTP Status Code Response:Not Found")
+        assert 404 == response.status, "Should Return a 404 HTTP Status Code Response:Not Found"
         # Should return error message
-        self.assertIn("Not Found", str(response.error), "Should return resource not found response")
+        assert "Not Found" in str(response.error), "Should return resource not found response"
