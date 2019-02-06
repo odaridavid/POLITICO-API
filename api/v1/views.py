78f06d1088085f1118fe5e8a08b55af4097e468c
Davidodari <davidkibzodari@gmail.com>
2019-02-06 14:28:56 +0300

    [Finishes #163684268] Create Get Political Parties Endpoint
    - Adds get party GET method to /parties endpoint in Blueprint and returns respective response
    - Refactors model by adding a method to get  list DT and add_party method takes a parameter
    - Tested in Postman

--- api/v1/views.py
+++ api/v1/views.py
@@ -6,36 +6,53 @@ from .models import OfficesModel
 version_1 = Blueprint('apiv1', __name__, url_prefix="/api/v1")
 
 
-@version_1.route("/parties", methods=['POST'])
-def api_create_party():
-    """
-    Creates a  party on parties endpoint with POST response
-    :return: response depending on post request data
-    """
-    # get the party as json and save it in the model
-    party = request.get_json(force=True)  # Ignore the mimetype and always try to parse JSON.
-    # Checks keys exist in given dict
-    if {'name', 'hqAddress', 'logoUrl'} <= set(party):
-        # Create Party Model instance and add Party to list
-        gen_id = PartiesModel(party).create_political_party()
-        if gen_id:
-            # successful response
+@version_1.route("/parties", methods=['GET', 'POST'])
+def api_parties():
+    if request.method == 'POST':
+        """
+        Creates a  party on parties endpoint with POST response
+        :return: response depending on post request data
+        """
+        # get the party as json and save it in the model
+        party = request.get_json(force=True)  # Ignore the mimetype and always try to parse JSON.
+        # Checks keys exist in given dict
+        if {'name', 'hqAddress', 'logoUrl'} <= set(party):
+            # Create Party Model instance and add Party to list
+            gen_id = PartiesModel().create_political_party(party)
+            if gen_id:
+                # successful response
+                data = {
+                    "status": 201,
+                    "data": [{
+                        "id": gen_id,
+                        "name": party['name']
+                    }]
+                }
+                return make_response(jsonify(data), 400)
+        else:
+            # Missing data bad request response
+            # Fail response
             data = {
-                "status": 201,
-                "data": [{
-                    "id": gen_id,
-                    "name": party['name']
-                }]
+                "status": 400,
+                "error": "400 ERROR:BAD REQUEST,Missing Key value"
             }
             return make_response(jsonify(data), 400)
-    else:
-        # Missing data bad request response
-        # Fail response
-        data = {
-            "status": 400,
-            "error": "400 ERROR:BAD REQUEST,Missing Key value"
-        }
-        return make_response(jsonify(data), 400)
+    elif request.method == 'GET':
+        """Handles Get request"""
+        parties = PartiesModel().get_all_political_parties()
+        if len(parties) >= 0:
+            # If parties list has no items or does
+            data = {
+                "status": 200,
+                "data": parties
+            }
+            return make_response(jsonify(data), 200)
+        else:
+            data = {
+                "status": 404,
+                "error": "404 ERROR:DATA NOT FOUND"
+            }
+            return make_response(jsonify(data), 404)
 
 
 @version_1.route("/offices", methods=['POST'])
