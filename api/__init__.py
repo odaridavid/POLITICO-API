5705c53ea90ab9d87398ed7b72d4db7cbef84064
Davidodari <davidkibzodari@gmail.com>
2019-02-07 01:09:32 +0300

    [Starts #163684270]Add Default error handler to display 404 error

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify
 # Setup Models
 from .v1 import models
 # Import blueprint - contain routes handling interaction between view and models
 from .v1.views import version_1
+
+
+# Error Handler Method
+def page_not_found(e):
+    error_response = {
+        "error": "404 ERROR:PAGE NOT FOUND",
+        "status": 404
+    }
+    return make_response(jsonify(error_response))
 
 
 # configure app prerequisites
@@ -11,5 +20,7 @@ def create_app():
     app = Flask(__name__)
     #  Register  blueprints in app instance creation
     app.register_blueprint(version_1)
+    # Error Handler for error message 404
+    app.register_error_handler(404, page_not_found)
     # Return application context
     return app
