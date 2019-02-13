066e2c2a7fd19c480919afd5aca8b4b1066d0ebf
David Odari <davidkibzodari@gmail.com>
2019-02-13 14:22:05 +0300

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -5,4 +5,4 @@ def generate_response(model_result):
     if 'Invalid Id' in model_result:
         return make_response(jsonify({"status": 404, "error": "Invalid Id Not Found"}), 404)
     # Fix For Delete Bug If Item Doesnt Exist- FIxes Deleting Twice
-    return make_response(jsonify({"status": 400, "error": "Item Not Found"}), 400)
+    return make_response(jsonify({"status": 404, "error": "Item Not Found"}), 404)
