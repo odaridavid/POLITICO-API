ec17d9ce31c8a083c7b1a70271b0af93545a9989
Davidodari <davidkibzodari@gmail.com>
2019-02-24 08:13:46 +0300

    [Chore #164185920] Refactor if statements

--- api/v2/views/results.py
+++ api/v2/views/results.py
@@ -10,17 +10,14 @@ result_api_v2 = Blueprint('results_v2', __name__, url_prefix="/api/v2")
 @jwt_required
 def api_results(office_id):
     oid = id_conversion(office_id)
-    if isinstance(oid, int):
-        results = ResultsModel(oid).get_results()
-    else:
+    if not isinstance(oid, int):
         return make_response(jsonify({"status": 400, "error": "Invalid Office Id"}), 400)
-
-    if isinstance(results, list):
-        return make_response(jsonify({"status": 200, "data": [{
-            "office": oid,
-            "candidate": results[0][0],
-            "results": results[0][1]
-        }]}), 200)
-    elif 'Empty' in results:
+    results = ResultsModel(oid).get_results()
+    if not isinstance(results, list):
         return make_response(jsonify({"status": 404, "error": "Results Not Found"}), 404)
-    return make_response(jsonify({"status": 404, "error": "Results Not Found"}), 404)
+    return make_response(jsonify({"status": 200,
+                                  "data": [{
+                                      "office": oid,
+                                      "candidate": results[0][0],
+                                      "results": results[0][1]
+                                  }]}), 200)
